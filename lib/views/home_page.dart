d6865071719340943f2af71e09bf8ff37eef8e4a
IkramZargar <ikramxargar@gmail.com>
2024-03-03 19:54:57 +0530

    appbar and globals directory

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -10,6 +10,27 @@ class homePage extends StatefulWidget {
 class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 60),
+          child: const Row(
+            children: [
+              Text('Portfolio'),
+              Spacer(),
+              Text('Home'),
+              SizedBox(width: 20,),
+              Text('About'),
+              SizedBox(width: 20,),
+              Text('Skills'),
+              SizedBox(width: 20,),
+              Text('Projects'),
+              SizedBox(width: 20,),
+              Text('Contact'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
