cba60336318bf7abcfc25c8e7588336e60eeefeb
IkramZargar <ikramxargar@gmail.com>
2024-03-04 16:10:33 +0530

    added animations

--- lib/globals/app_colors.dart
+++ lib/globals/app_colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColor{
-  static Color bgColor = Color(0xff002147);
-  static Color themeColor = Color(0xff007BA7);
+  static Color bgColor = const Color(0xff002147);
+  static Color themeColor = const Color(0xff007BA7);
+  static Color aqua = const Color(0xff0FFFFF);
+  static Color lawGreen = const Color(0xff7CFC00);
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/globals/app_assets.dart';
@@ -76,79 +77,100 @@ class _HomePageState extends State<HomePage> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Hello It`s Me',
-                  style: AppTextStyles.heroTextStyle(),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  'Ikram Zargar',
-                  style: AppTextStyles.headingTextStyle(),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'And I`m a ',
-                      style: AppTextStyles.heroTextStyle(),
-                    ),
-                    AnimatedTextKit(
-                      animatedTexts: [
-                        TyperAnimatedText(
-                          'Flutter Developer',
-                          textStyle: AppTextStyles.heroTextStyle().copyWith(
-                            color: Colors.green,
-                          ),
-                        ),
-                      ],
-                      pause: const Duration(milliseconds: 1000),
-                      displayFullTextOnTap: true,
-                      stopPauseOnTap: true,
-                      repeatForever: true,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                SizedBox(
-                  width: size.width * 0.5,
+                FadeInDown(
+                  duration: const Duration(milliseconds: 1200),
                   child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipisci elit,'
-                    'sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
-                    'Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,'
-                    'nisi ut aliquid ex ea commodi consequatur.',
-                    style: AppTextStyles.normalStyle(),
+                    'Hello It`s Me',
+                    style: AppTextStyles.heroTextStyle(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FadeInRight(
+                  duration: const Duration(milliseconds: 1400),
+                  child: Text(
+                    'Ikram Zargar',
+                    style: AppTextStyles.headingTextStyle(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FadeInLeft(
+                  duration: const Duration(milliseconds: 1400),
+                  child: Row(
+                    children: [
+                      Text(
+                        'And I`m a ',
+                        style: AppTextStyles.heroTextStyle(),
+                      ),
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          TyperAnimatedText(
+                            'Flutter Developer',
+                            textStyle: AppTextStyles.heroTextStyle().copyWith(
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                        pause: const Duration(milliseconds: 1000),
+                        displayFullTextOnTap: true,
+                        stopPauseOnTap: true,
+                        repeatForever: true,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FadeInDown(
+                  duration: const Duration(milliseconds: 1600),
+                  child: SizedBox(
+                    width: size.width * 0.5,
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipisci elit,'
+                      'sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+                      'Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,'
+                      'nisi ut aliquid ex ea commodi consequatur.',
+                      style: AppTextStyles.normalStyle(),
+                    ),
                   ),
                 ),
                 const SizedBox(
                   height: 22,
                 ),
-                Row(
-                  children: [
-                    buildSocialButtons(asset: AppAssets.facebook),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    buildSocialButtons(asset: AppAssets.twitter),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    buildSocialButtons(asset: AppAssets.linkedin),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    buildSocialButtons(asset: AppAssets.insta),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    buildSocialButtons(asset: AppAssets.github),
-                  ],
-                )
+                FadeInUp(
+                  duration: const Duration(milliseconds: 1600),
+                  child: Row(
+                    children: [
+                      buildSocialButtons(asset: AppAssets.facebook),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      buildSocialButtons(asset: AppAssets.twitter),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      buildSocialButtons(asset: AppAssets.linkedin),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      buildSocialButtons(asset: AppAssets.insta),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      buildSocialButtons(asset: AppAssets.github),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                FadeInUp(
+                    duration: const Duration(milliseconds: 1800),
+                    child: buildMaterialButton(onTap: () {}))
               ],
             )
           ],
@@ -157,19 +179,68 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  CircleAvatar buildSocialButtons({required String asset}) {
-    return CircleAvatar(
-      maxRadius: 20,
-      backgroundColor: AppColor.themeColor,
-      child: CircleAvatar(
-        maxRadius: 18,
-        backgroundColor: AppColor.bgColor,
-        child: Image.asset(
-          asset,
-          width: 20,
-          height: 24,
-          color: AppColor.themeColor,
-          fit: BoxFit.contain,
+  MaterialButton buildMaterialButton({required VoidCallback onTap}) {
+    return MaterialButton(
+      onPressed: onTap,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      color: AppColor.themeColor,
+      shape: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide.none,
+      ),
+      height: 46,
+      minWidth: 120,
+      hoverColor: AppColor.aqua,
+      elevation: 6,
+      splashColor: AppColor.lawGreen,
+      focusElevation: 12,
+      child: Text('Download CV', style: AppTextStyles.headerTextStyle()),
+    );
+  }
+  // Ink buildSocialButtons({required String asset, required bool hover}) {
+  //   return Ink(
+  //     width: 45,
+  //     height: 45,
+  //     decoration: BoxDecoration(
+  //       border: Border.all(color: AppColor.themeColor, width: 2.0),
+  //       color: AppColor.bgColor,
+  //       shape: BoxShape.circle,
+  //     ),
+  //     padding: const EdgeInsets.all(6),
+  //     child: Image.asset(
+  //       asset,
+  //       width: 10,
+  //       height: 12,
+  //       color: hover ? AppColor.bgColor : AppColor.themeColor,
+  //       // fit: BoxFit.fill,
+  //     ),
+  //   );
+  // }
+
+  Ink buildSocialButtons({required String asset}) {
+    return Ink(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.green, width: 3.0),
+        color: Colors.greenAccent,
+        shape: BoxShape.circle,
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(500.0),
+        splashColor: AppColor.lawGreen,
+        hoverColor: AppColor.aqua,
+        onTap: () {},
+        child: CircleAvatar(
+          maxRadius: 18,
+          backgroundColor: AppColor.bgColor,
+          child: Image.asset(
+            asset,
+            width: 20,
+            height: 24,
+            color: AppColor.themeColor,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
