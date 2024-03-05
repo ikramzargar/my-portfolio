32504d2102249279292d610792ff1f42be5b6a17
IkramZargar <ikramxargar@gmail.com>
2024-03-05 09:36:48 +0530

    completed about me

--- lib/views/about_me.dart
+++ lib/views/about_me.dart
@@ -19,13 +19,19 @@ class _AboutMeState extends State<AboutMe> {
     final Size size = MediaQuery.of(context).size;
     return Container(
       height: size.height,
+      width: size.width,
       color: AppColor.bgColor2,
+      alignment: Alignment.center,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.asset(
-            AppAssets.profile2,
-            height: 450,
-            width: 400,
+          FadeInRight(
+            duration: Duration(milliseconds: 1200),
+            child: Image.asset(
+              AppAssets.profile2,
+              height: 450,
+              width: 400,
+            ),
           ),
           SizedBox(
             width: 25,
@@ -36,7 +42,7 @@ class _AboutMeState extends State<AboutMe> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 FadeInRight(
-                  duration: const Duration(milliseconds: 1400),
+                  duration: const Duration(milliseconds: 1200),
                   child: RichText(
                     text: TextSpan(
                       text: 'About',
@@ -55,20 +61,26 @@ class _AboutMeState extends State<AboutMe> {
                 const SizedBox(
                   height: 6,
                 ),
-                Text(
-                  'Flutter Developer!',
-                  style: AppTextStyles.heroTextStyle(),
+                FadeInLeft(
+                  duration: Duration(milliseconds: 1400),
+                  child: Text(
+                    'Flutter Developer!',
+                    style: AppTextStyles.heroTextStyle(),
+                  ),
                 ),
                 const SizedBox(
                   height: 8,
                 ),
-                Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
-                  'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
-                  'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum '
-                  'dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'
-                  'mollit anim id est laborum.',
-                  style: AppTextStyles.normalStyle(),
+                FadeInLeft(
+                  duration: Duration(milliseconds: 1600),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
+                    'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+                    'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum '
+                    'dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'
+                    'mollit anim id est laborum.',
+                    style: AppTextStyles.normalStyle(),
+                  ),
                 ),
                 const SizedBox(
                   height: 15,
