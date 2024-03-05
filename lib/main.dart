dff3e475db1d5b05d14f152339ee004fc32577c8
IkramZargar <ikramxargar@gmail.com>
2024-03-05 15:12:04 +0530

    added helper

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/views/home_page.dart';
+import 'package:my_portfolio/views/dashboard.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const Dashboard(),
     );
   }
 }
---
+++ lib/views/dashboard.dart
@@ -0,0 +1,89 @@
+  import 'package:flutter/material.dart';
+import 'package:my_portfolio/views/home_page.dart';
+  import '../globals/app_colors.dart';
+  import '../globals/app_text_styles.dart';
+
+  class Dashboard extends StatefulWidget {
+    const Dashboard({super.key});
+
+    @override
+    State<Dashboard> createState() => _DashboardState();
+  }
+
+  class _DashboardState extends State<Dashboard> {
+    final onMenuHover = Matrix4.identity()..scale(1.0);
+    final menuItems = <String>[
+      'Home',
+      'About',
+      'Skills',
+      'My Projects',
+      'Contact',
+    ];
+    var menuIndex = 0;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        //backgroundColor: AppColor.bgColor,
+        appBar: AppBar(
+          toolbarHeight: 90,
+          titleSpacing: 100,
+          backgroundColor: AppColor.bgColor,
+          elevation: 0,
+          title: Row(
+            children: [
+              Text(
+                'Portfolio',
+                style: AppTextStyles.headerTextStyle(),
+              ),
+              const Spacer(),
+              SizedBox(
+                height: 30,
+                child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      borderRadius: BorderRadius.circular(100),
+                      onHover: (value) {
+                        setState(() {
+                          if (value) {
+                            menuIndex == index;
+                          } else {
+                            menuIndex = 0;
+                          }
+                        });
+                      },
+                      child:
+                          buildNavBar(index, menuIndex == index ? true : false),
+                    );
+                  },
+                  separatorBuilder: (context, child) => SizedBox(
+                    width: 8,
+                  ),
+                  itemCount: menuItems.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: HomePage(),
+      );
+    }
+
+    AnimatedContainer buildNavBar(int index, bool hover) {
+      return AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        alignment: Alignment.center,
+        width: hover ? 80 : 75,
+        transform: hover ? onMenuHover : null,
+        child: Text(
+          menuItems[index],
+          style: AppTextStyles.headerTextStyle().copyWith(
+            color: hover ? AppColor.themeColor : Colors.white,
+          ),
+        ),
+      );
+    }
+  }
