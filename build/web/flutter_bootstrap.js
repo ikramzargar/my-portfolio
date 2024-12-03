30a442efb054f31484c985b6075f9ea478cce8fa
ikramzargar <ikramxargar@gmail.com>
2024-12-03 15:10:47 +0530

    favicon

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1709345358"
+    serviceWorkerVersion: "3321138402"
   }
 });
